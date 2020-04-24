79f08218ec18b3ddd221afcfadf31f5e8e5d7980
unknown <officialspyme@gmail.com>
2020-04-24 23:48:52 +0400

    Update urls

--- Main/urls.py
+++ Main/urls.py
@@ -21,5 +21,8 @@ from . import views
 app_name = "Main"
 
 urlpatterns = [
-    path("", views.homepage, name="homepage"),
+    path("", views.home_page, name="home_page"),
+    path("logout/", views.logout_page, name="log_out"),
+    path("login/", views.login_page, name="login_page"),
+    path("register/", views.register_page, name="register_page"),
 ]
