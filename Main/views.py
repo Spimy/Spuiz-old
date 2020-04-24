dae07e19685eedc3b6eb31e20b20fd4e657da6b2
unknown <officialspyme@gmail.com>
2020-04-24 23:49:13 +0400

    Add login and register views

--- Main/views.py
+++ Main/views.py
@@ -1,5 +1,65 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import user_passes_test
+
+from .models import *
+from .forms import RegistrationForm
+
+
+def authenticated_check(user):
+    return not user.is_authenticated
 
 # Create your views here.
-def homepage(request):
+def home_page(request):
     return render(request, "homepage.html")
+
+def login_page(request):
+    
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            
+            if user is not None:
+                messages.success(request, "You have successfully logged in.")
+                login(request, user)
+                return redirect("Main:home_page")
+            else:
+                for msg in form.error_messages:
+                    messages.error(request, f"{msg.upper()}: {form.error_messages[msg]}")
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg.upper()}: {form.error_messages[msg]}")
+    
+    form = AuthenticationForm()
+    return render(request,"login.html", context={"form": form})
+    
+@user_passes_test(authenticated_check, login_url="/")
+def register_page(request):
+    
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, "You have successfully logged in.")
+            login(request, user)
+        
+            return redirect("Main:home_page")
+        else:
+            for msg in form.error_messages:
+                messages.error(request, f"{msg.upper()}: {form.error_messages[msg]}.")
+    
+    form = RegistrationForm()
+    return render(request, "register.html", context={"form": form})
+    
+def logout_page(request):
+    logout(request)
+    messages.info(request, "You have been logged out.")
+    return HttpResponseRedirect(request.GET.get("next", "/"))
