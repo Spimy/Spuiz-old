5787c1560ab94f06973d1f7d70683c2d79d0bb50
unknown <officialspyme@gmail.com>
2020-04-26 22:21:25 +0400

    Remove register help text

--- Main/forms.py
+++ Main/forms.py
@@ -5,12 +5,19 @@ from django.contrib.auth.models import User
 
 # Create your forms here
 class RegistrationForm(UserCreationForm):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        for field_name in ["username", "email", "password1", "password2"]:
+            self.fields[field_name].help_text = None
+    
     email = forms.EmailField(required=True, label="Email", max_length=255, widget=forms.EmailInput(attrs={'autofocus': True}))
     
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
-        
+
     def save(self, commit=True):
         user = super(RegistrationForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
