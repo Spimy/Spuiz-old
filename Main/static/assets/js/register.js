050e8112c4e0402d6b22674ede19c62a3de2abcc
unknown <officialspyme@gmail.com>
2020-04-26 22:23:34 +0400

    Add and style register page

---
+++ Main/static/assets/js/register.js
@@ -0,0 +1,4 @@
+document.getElementsByName("username")[0].placeholder = "\uf007";
+document.getElementsByName("email")[0].placeholder = "\uf0e0";
+document.getElementsByName("password1")[0].placeholder = "\uf023";
+document.getElementsByName("password2")[0].placeholder = "\uf023";
