02d04af23bfe18eb4851d3d6d0a6f7085eb91593
unknown <officialspyme@gmail.com>
2020-05-04 21:43:37 +0400

    Update error messages and quiz slug view

--- Main/views.py
+++ Main/views.py
@@ -1,35 +1,20 @@
-import re
+import json
+import random
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 
 from .models import *
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 
-
-def validate_password_strength(value):
-    
-    min_length = 8
-
-    if len(value) < min_length:
-        return f"Password must be at least {min_length} characters long."
-    
-    if (not re.match(r"^(?=.*[a-zA-Z])(?=.*[0-9]){{{0},}}".format(min_length), value)):
-        return "Password must contain both letters and digits."
-
-    # check for uppercase letter
-    if not any(c.isupper() for c in value):
-        return "Password must contain at least 1 uppercase letter."
-
-    return value
 
 def user_slug(request, user_slug):
+    
     users = [u.slug for u in UserProfile.objects.all()]
     if user_slug in users:
 
-        matching_quizzes = Quiz.objects.filter(user_profile__slug=user_slug)
+        matching_quizzes = Quiz.objects.filter(author__user_profile__slug=user_slug)
         quiz_urls = {}
 
         for quiz in matching_quizzes.all():
@@ -38,13 +23,29 @@ def user_slug(request, user_slug):
         return render(request, "user_profile.html", context={"quizzes": quiz_urls})
 
 def quiz_slug(request, user_slug, quiz_slug):
+    
     users = [u.slug for u in UserProfile.objects.all()]
     if user_slug in users:
 
-        matching_quizzes = Quiz.objects.filter(user_profile__slug=user_slug)
-        selected_quiz = matching_quizzes.filter(slug=quiz_slug)
+        matching_quizzes = Quiz.objects.filter(author__user_profile__slug=user_slug)
+        selected_quiz = matching_quizzes.filter(slug=quiz_slug)[0]
+        
+        flatten = lambda l: [item for sublist in l for item in sublist]
+        questions = {}
+        
+        for question in selected_quiz.questions.all():
+            answers = []
+            answers.append(question.correct.all())
+            answers.append(question.wrong.all())
+            
+            answers = flatten(answers)
+            random.shuffle(answers)
+            questions[question] = answers
+            
+        quiz = {}
+        quiz[selected_quiz] = questions
 
-        return render(request, "quiz_page.html", context={"quiz": selected_quiz})
+        return render(request, "quiz_page.html", context={"quiz": quiz})
 
 # Create your views here.
 def home_page(request):
@@ -57,7 +58,7 @@ def login_page(request):
         return redirect("Main:home_page")
     
     if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
+        form = LoginForm(request, data=request.POST)
         
         if form.is_valid():
             username = form.cleaned_data.get("username")
@@ -75,7 +76,7 @@ def login_page(request):
             for msg in form.error_messages:
                 messages.error(request, f"{msg.upper()}: {form.error_messages[msg]}")
     
-    form = AuthenticationForm()
+    form = LoginForm()
     return render(request,"login.html", context={"form": form})
 
 def register_page(request):
@@ -88,28 +89,23 @@ def register_page(request):
         
         if form.is_valid():
             user = form.save()
-            messages.success(request, "You have successfully logged in.")
+            messages.success(request, "You have successfully registered and logged in.")
             login(request, user)
         
             return redirect("Main:home_page")
         else:
-            
-            username = form.cleaned_data.get("username")
-            password = form.cleaned_data.get("password1")
-            
-            if username is None:
-                messages.error(request, "Username must not contain any spaces.")
-            
-            if (password == username):
-                 messages.error(request, "Password should not be the same as username.")
-            
-            validation = validate_password_strength(password)
-            
-            if (validation != password):
-                messages.error(request, validation)
-            
-            for msg in form.error_messages:
-                messages.error(request, f"{msg.upper()}: {form.error_messages[msg]}.")
+            # for msg in form.error_messages:
+            #     messages.error(request, f"{msg.upper()}: {form.error_messages[msg]}.")
+            errors = json.loads(form.errors.as_json())
+            print(errors)
+            for msg in errors:
+                
+                error_msg = errors[msg][0]['message']
+                
+                if errors[msg][0]['code'] != "":
+                    error_msg = errors[msg][0]['code'].upper() + ": " + error_msg
+                    
+                messages.error(request, error_msg)
     
     form = RegistrationForm()
     return render(request, "register.html", context={"form": form})
