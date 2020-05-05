f4df1251ce5ba598aaae9d26329641e52b1118d6
unknown <officialspyme@gmail.com>
2020-05-05 15:16:09 +0400

    Handle correct answer

--- Main/views.py
+++ Main/views.py
@@ -7,45 +7,8 @@ from django.contrib.auth import login, logout, authenticate
 
 from .models import *
 from .forms import RegistrationForm, LoginForm
+from django.http.response import HttpResponse
 
-
-def user_slug(request, user_slug):
-    
-    users = [u.slug for u in UserProfile.objects.all()]
-    if user_slug in users:
-
-        matching_quizzes = Quiz.objects.filter(author__user_profile__slug=user_slug)
-        quiz_urls = {}
-
-        for quiz in matching_quizzes.all():
-            quiz_urls[quiz] = quiz.slug
-
-        return render(request, "user_profile.html", context={"quizzes": quiz_urls})
-
-def quiz_slug(request, user_slug, quiz_slug):
-    
-    users = [u.slug for u in UserProfile.objects.all()]
-    if user_slug in users:
-
-        matching_quizzes = Quiz.objects.filter(author__user_profile__slug=user_slug)
-        selected_quiz = matching_quizzes.filter(slug=quiz_slug)[0]
-        
-        flatten = lambda l: [item for sublist in l for item in sublist]
-        questions = {}
-        
-        for question in selected_quiz.questions.all():
-            answers = []
-            answers.append(question.correct.all())
-            answers.append(question.wrong.all())
-            
-            answers = flatten(answers)
-            random.shuffle(answers)
-            questions[question] = answers
-            
-        quiz = {}
-        quiz[selected_quiz] = questions
-
-        return render(request, "quiz_page.html", context={"quiz": quiz})
 
 # Create your views here.
 def home_page(request):
@@ -114,3 +77,59 @@ def logout_page(request):
     logout(request)
     messages.info(request, "You have been logged out.")
     return HttpResponseRedirect(request.GET.get("next", "/"))
+
+def user_slug(request, user_slug):
+    
+    users = [u.slug for u in UserProfile.objects.all()]
+    if user_slug in users:
+
+        matching_quizzes = Quiz.objects.filter(author__user_profile__slug=user_slug)
+        quiz_urls = {}
+
+        for quiz in matching_quizzes.all():
+            quiz_urls[quiz] = quiz.slug
+
+        return render(request, "user_profile.html", context={"quizzes": quiz_urls})
+
+def quiz_slug(request, user_slug, quiz_slug):
+    
+    users = [u.slug for u in UserProfile.objects.all()]
+    if user_slug in users:
+
+        matching_quizzes = Quiz.objects.filter(author__user_profile__slug=user_slug)
+        selected_quiz = matching_quizzes.filter(slug=quiz_slug)[0]
+        
+        if request.method == "POST":
+            correct = 0
+            for key, answer in request.POST.items():
+                if key == "csrfmiddlewaretoken": continue
+                
+                for question in selected_quiz.questions.all():
+                    if key.lower() == question.question.lower():
+                        
+                        answers = question.correct.values("answer")
+                        answers = [ans["answer"] for ans in answers]
+                        
+                        if answer in answers:
+                            correct += 1
+                            
+            return HttpResponse(correct)
+        
+        flatten = lambda l: [item for sublist in l for item in sublist]
+        questions = {}
+        
+        for question in selected_quiz.questions.all():
+            answers = []
+            answers.append([question.correct.first()])
+            answers.append(question.wrong.all())
+            
+            answers = flatten(answers)
+            random.shuffle(answers)
+            questions[question] = answers
+            
+        quiz = {
+            "questions": questions,
+            "quiz_info": selected_quiz
+            }
+
+        return render(request, "quiz_page.html", context={"quiz": quiz})
