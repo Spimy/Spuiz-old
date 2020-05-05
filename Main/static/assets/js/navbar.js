0f52b865df3fd0df37bac25d38b0c61ecaa68582
unknown <officialspyme@gmail.com>
2020-05-05 23:55:35 +0400

    Update navbar

--- Main/static/assets/js/navbar.js
+++ Main/static/assets/js/navbar.js
@@ -3,7 +3,7 @@ const navbar = document.getElementById("sticky_nav");
 document.addEventListener("scroll", () => {
     
     if (window.scrollY > 1) {
-        navbar.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+        navbar.style.backgroundColor = "rgba(0, 0, 0, 0.6)";
     } else {
         navbar.style.backgroundColor = "rgba(0, 0, 0, 0)";
     }
