8c98088646baca119e6705e3af039f773b92a87e
unknown <officialspyme@gmail.com>
2020-05-06 21:51:45 +0400

    Add ajax requests

--- Main/static/assets/js/quiz_page.js
+++ Main/static/assets/js/quiz_page.js
@@ -1,5 +1,6 @@
 // =====================================================
 // Check for viewport to change opacity and scale of question card
+// =====================================================
 const getElementsInArea = (function(docElm) {
     let viewportHeight = docElm.clientHeight;
 
@@ -50,4 +51,58 @@ function f(e){
         zone        : [40, 40] // percentage distance from top & bottom
     });
 }
+// =====================================================
+
+
+// =====================================================
+// AJAX
+// =====================================================
+
+const quiz_info_form = document.getElementsByClassName("quiz-info")[0];
+const quiz_info_btns = quiz_info_form.getElementsByClassName("quiz-info-btns")[0];
+const updownvote_btns = quiz_info_form.getElementsByClassName("quiz-updownvote");
+
+quiz_info_form.addEventListener("submit", event => {
+
+    event.preventDefault();
+
+    const btn = document.activeElement;
+    const token = quiz_info_form.getElementsByTagName("input")[0].value;
+    const vote = btn.value;
+
+    const data = new FormData();
+    data.append("csrfmiddlewaretoken", token);
+    data.append("vote", vote);
+    
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", window.location.href, true);
+    xhr.setRequestHeader("X-CSRFToken", token);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.send(data);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+
+                const wrapper = document.createElement("div");
+                wrapper.innerHTML = JSON.parse(xhr.responseText)["updownvotebtns"];
+                
+                const quiz_info_btns_dom = wrapper.getElementsByClassName("quiz-info-btns")[0];
+                quiz_info_btns.innerHTML = quiz_info_btns_dom.innerHTML;
+
+            } else {
+
+                const msgs = document.getElementsByClassName("msgs")[0];
+                const wrapper = document.createElement("div");
+
+                wrapper.innerHTML = JSON.parse(xhr.responseText)["msg"];
+                msgs.innerHTML = wrapper.getElementsByClassName("msgs")[0].innerHTML
+                messageEvent("msg-error", 10);
+                
+            }
+        }  
+    }
+
+});
+
 // =====================================================
