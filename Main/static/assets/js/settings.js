ae73a9bb8b8c9647bee7fd8d9ba15fd1ca09b01c
Spimy <officialspyme@gmail.com>
2020-05-20 20:41:31 +0400

    Ajax request for avatar and banner upload

--- Main/static/assets/js/settings.js
+++ Main/static/assets/js/settings.js
@@ -86,6 +86,7 @@ save_btn.addEventListener("click", event => {
                 const msgs = document.getElementsByClassName("msgs")[0];
                 msgs.innerHTML = data.msg;
                 messageEvent("msg-success", 3);
+
                 original_bio = simplemde.value();
                 save_btn.removeAttribute("style");
             });
@@ -94,4 +95,68 @@ save_btn.addEventListener("click", event => {
 
 });
 
+// =====================================================
+
+
+// =====================================================
+// AJAX for avatar and banner upload
+// =====================================================
+
+const ajaxUpload = (form_id, avatar=false) => {
+    
+    const form = document.getElementById(form_id);
+    const input = form.getElementsByTagName("input")[1];
+    const img = form.getElementsByTagName("img")[0]
+    
+    if (input.files.length > 0) {
+
+        const dropbox = input.parentElement;
+        
+        dropbox.style.color = "var(--purple-normal)";
+        dropbox.style.outline = "2px dashed var(--purple-normal)";
+        
+        const data = new FormData(form);
+        
+        fetch(window.location.href, {
+            method: "POST",
+            headers: {
+                "X-CSRFToken": data.get("csrfmiddlewaretoken"),
+                "X-Requested-With": "XMLHttpRequest"
+            },
+            body: data
+        }).then(async res => {
+            if (res.status === 200) {
+                res.json().then(data => {
+                    const msgs = document.getElementsByClassName("msgs")[0];
+                    msgs.innerHTML = data.msg;
+                    
+                    const wrapper = document.createElement("div");
+                    wrapper.innerHTML = data.settings;
+                    
+                    if (avatar) {
+                        const navbar_avatar = document.querySelector(".avatar-container > img");
+                        const navbar_avatar_dom = wrapper.querySelector(".avatar-container > img");
+                        navbar_avatar.src = navbar_avatar_dom.src;
+                    }
+                    
+                    const form_dom = wrapper.getElementsByClassName(form_id)[0];
+                    const img_dom = form_dom.getElementsByTagName("img")[0];
+                    img.src = img_dom.src;
+                    
+                    messageEvent("msg-success", 3);
+                    dropbox.removeAttribute("style");
+                });
+            }
+        });
+    }
+    
+}
+
+const avatar_form = document.getElementById("avatar-form");
+const avatar_input = avatar_form.getElementsByTagName("input")[1];
+avatar_input.addEventListener("change", () => { ajaxUpload("avatar-form", true) });
+
+const banner_form = document.getElementById("banner-form");
+const banner_input = banner_form.getElementsByTagName("input")[1];
+banner_input.addEventListener("change", () => { ajaxUpload("banner-form") });
 // =====================================================
