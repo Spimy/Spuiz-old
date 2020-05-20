9ff6d58e776a02f8fdebacc5426f1556e6ad8ef3
Spimy <officialspyme@gmail.com>
2020-05-20 23:52:01 +0400

    Add UserEditForm

--- Main/forms.py
+++ Main/forms.py
@@ -2,8 +2,12 @@ import re
 from difflib import SequenceMatcher as SM
 
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    AuthenticationForm,
+    UserChangeForm,
+    PasswordChangeForm
+)
 from django.contrib.auth.models import User
 
 
@@ -94,3 +98,80 @@ class LoginForm(AuthenticationForm):
         
     username = forms.CharField(label="Username", widget=forms.widgets.TextInput(attrs={"placeholder": "\uf007"}))
     password = forms.CharField(label="Password", widget=forms.widgets.PasswordInput(attrs={"placeholder": "\uf023"}))
+    
+class UserEditForm(forms.ModelForm):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = kwargs.pop("instance", None)
+        self.label_suffix = ""
+    
+    class Meta:
+        model = User
+        fields = ("username", "email")
+    
+    current_password_flag = True
+    
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder": "\uf007"}))
+    email = forms.EmailField(required=True, label="Email", max_length=255, 
+                             widget=forms.EmailInput(attrs={
+                                 "autofocus": True,
+                                 "placeholder": "\uf0e0"
+                                 })
+                             )
+    password = forms.CharField(label="New Password", widget=forms.PasswordInput(attrs={"placeholder": "\uf023"}), required=False)
+    password_confirm = forms.CharField(label="New Password confirmation", widget=forms.PasswordInput(attrs={"placeholder": "\uf023"}), required=False)
+    current_password = forms.CharField(label="Current Password Confirmation", widget=forms.PasswordInput(attrs={"placeholder": "\uf023"}))
+    
+    def set_current_password_flag(self): 
+        self.current_password_flag = False
+        return 0
+    
+    def clean(self):
+        cleaned_data = super(UserEditForm, self).clean()
+        
+        username = self.cleaned_data.get("username")
+        if User.objects.filter(username=username).exists():
+            user_with_username = User.objects.get(username=username)
+            if self.user != user_with_username:
+                raise forms.ValidationError("Username already taken")
+        
+        email = self.cleaned_data.get("email")
+        if User.objects.filter(email=email).exists():
+            user_with_email = User.objects.get(email=email)
+            if self.user != user_with_email:
+                raise forms.ValidationError("An account has already been registered with given email")
+        
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password1")
+        
+        if password and password_confirm:
+            if password != password_confirm:
+                raise forms.ValidationError("Passwords do not match.")
+        
+        if password:
+            if (SM(None, password, username).ratio() * 100) >= 80:
+                print("password test")
+                raise forms.ValidationError("Password is too similar to username")
+                
+            password = validate_password_strength(password)
+
+        current_password = self.cleaned_data.get('current_password')
+
+        if not current_password:
+            raise forms.ValidationError("You must enter your old password.")
+
+        if self.current_password_flag == False:
+            raise forms.ValidationError("Incorrect current password provided.")
+            
+        return cleaned_data
+        
+    def save(self, commit=True):
+        user = super(UserEditForm, self).save(commit=False)
+        password = self.cleaned_data.get("password")
+        if password:
+            user.set_password(password)
+            user.save()
+        if commit:
+            user.save()
+        return user
