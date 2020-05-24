46417697904eb106a5c0a53f4bfc32b5dfe5a35d
Spimy <officialspyme@gmail.com>
2020-05-24 16:02:39 +0400

    Show newest by sorting my primary key id

--- Main/views.py
+++ Main/views.py
@@ -101,7 +101,7 @@ def calculate_percentage_score(score, total_score):
 
 # Create your views here.
 def home_page(request):
-    newest_quizzes = Quiz.objects.all()[::-1][:10]
+    newest_quizzes = Quiz.objects.all().order_by("-pk")[:10]
     top_quizzes = Quiz.objects.annotate(up_count=Count("upvotes")).order_by("-up_count")[:10]
     return render(request, "homepage.html", context={"newest_quizzes": newest_quizzes, 
                                                      "top_quizzes": top_quizzes})
