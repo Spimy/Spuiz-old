8b80edfe3d998c610ce926aecab2728071e42375
Spimy <officialspyme@gmail.com>
2020-05-26 13:21:39 +0400

    Add notifications model and handler via signals

--- Main/models.py
+++ Main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.contrib.postgres.fields import ArrayField
 
 
@@ -67,6 +67,16 @@ class CompletedQuiz(models.Model):
     class Meta:
             verbose_name_plural = "Completed Quizzes"
 
+class Notification(models.Model):
+    title = models.CharField(max_length=255)
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+    for_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="for_user", null=True)
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="from_user", null=True)
+    
+    def __str__(self):
+        return f"\"{self.title}\" notification for {self.for_user.username}"
+    
 class UserProfile(models.Model):
     
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_profile")
@@ -86,7 +96,45 @@ class UserProfile(models.Model):
 
 
 def create_profile(sender, **kwargs):
-    if kwargs["created"]:
+    if kwargs.get("created", False):
         user_profile = UserProfile.objects.create(user=kwargs["instance"])
         
+def noticiations_manager(boolean, author, title, message):
+    if boolean:
+        user_profiles = UserProfile.objects.all()
+        
+        for user_profile in user_profiles:
+            
+            if user_profile.user == author: 
+                continue
+            
+            if author in user_profile.following.all():
+                Notification.objects.create(title=title,
+                                            message=message,
+                                            for_user=user_profile.user,
+                                            from_user=author)
+    
+def create_quiz(sender, instance, **kwargs):
+    noticiations_manager(kwargs.get("created", False),
+                          instance.author,
+                          "New Quiz",
+                          f"{instance.author.username} has created a new quiz titled \"{instance.title}\"!")
+
+def delete_quiz(sender, instance, **kwargs):
+    noticiations_manager(True,
+                          instance.author,
+                          "Deleted Quiz",
+                          f"{instance.author.username} has delete a quiz titled \"{instance.title}\"!")
+
+def complete_quiz(sender, instance, **kwargs):
+    noticiations_manager(kwargs.get("created", False),
+                          instance.user,
+                          "Completed Quiz",
+                          f"{instance.user.username} has completed a quiz titled \"{instance.quiz.title}\"!")
+
 post_save.connect(create_profile, User)
+
+post_save.connect(create_quiz, Quiz)
+post_delete.connect(delete_quiz, Quiz)
+
+post_save.connect(complete_quiz, CompletedQuiz)
