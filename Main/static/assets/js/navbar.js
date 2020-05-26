45c2c54d14e999cdbe81a4a481d49dddcdf43416
Spimy <officialspyme@gmail.com>
2020-05-26 22:26:22 +0400

    Check screen size

--- Main/static/assets/js/navbar.js
+++ Main/static/assets/js/navbar.js
@@ -11,32 +11,36 @@ document.addEventListener("scroll", () => {
 });
 
 const hideBtns = () => {
-    
-    const nav_right = document.getElementById("sticky_nav").getElementsByClassName("nav-right")[0];
-    const unlogged_btns = document.getElementById("sticky_nav").getElementsByClassName("unlogged");
-    
 
-    nav_right.style.marginRight = "-500px";
-    setTimeout(() => {
-        if (unlogged_btns) {
-            for (const btn of unlogged_btns) {
-                btn.style.display = "none";
+    if (window.screen.width < 750) {
+
+        const nav_right = document.getElementById("sticky_nav").getElementsByClassName("nav-right")[0];
+        const unlogged_btns = document.getElementById("sticky_nav").getElementsByClassName("unlogged");
+        
+    
+        nav_right.style.marginRight = "-500px";
+        setTimeout(() => {
+            if (unlogged_btns) {
+                for (const btn of unlogged_btns) {
+                    btn.style.display = "none";
+                }
             }
-        }
-    }, 200);
-
+        }, 200);
+    }
 
 }
 
 const showBtns = () => {
 
-    const nav_right = document.getElementById("sticky_nav").getElementsByClassName("nav-right")[0];
-    const unlogged_btns = document.getElementById("sticky_nav").getElementsByClassName("unlogged");
-    if (unlogged_btns) {
-        for (const btn of unlogged_btns) {
-            btn.removeAttribute("style");
+    if (window.screen.width < 750) {
+        const nav_right = document.getElementById("sticky_nav").getElementsByClassName("nav-right")[0];
+        const unlogged_btns = document.getElementById("sticky_nav").getElementsByClassName("unlogged");
+        if (unlogged_btns) {
+            for (const btn of unlogged_btns) {
+                btn.removeAttribute("style");
+            }
         }
+        nav_right.removeAttribute("style");
     }
-    nav_right.removeAttribute("style");
 
 }
