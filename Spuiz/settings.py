b1708cddfbbdb91e10e73d63eba51ba92ad6efa0
Spimy <officialspyme@gmail.com>
2020-05-26 13:24:15 +0400

    Load global notification check context processor

--- Spuiz/settings.py
+++ Spuiz/settings.py
@@ -95,6 +95,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'Main.context_processors.has_notifications'
             ],
         },
     },
